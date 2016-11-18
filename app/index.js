b9d468c8a5baf20010ae84ff92409a77f3dde6e8
James Helton <trealtrix@gmail.com>
2016-11-17 21:49:34 -0500

    Implement ReactRouter

--- app/index.js
+++ app/index.js
@@ -3,8 +3,17 @@ var ReactDOM = require('react-dom');
 // var routes = require('config/routes');
 var Test = require('components/Test');
 
+var ReactRouter = require( 'react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+import { browserHistory } from 'react-router';
+
+
 console.log(Test)
 ReactDOM.render(
-  <Test />,
+  <Router history={browserHistory}>
+    <Route path='/' component={Test}>
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
