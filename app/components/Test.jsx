26f0b220ec87366ed6e0a911f08ecba3842e0527
James Helton <trealtrix@gmail.com>
2016-11-17 21:09:11 -0500

    Add basic react component

---
+++ app/components/Test.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Test = React.createClass({
+  render: function() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+});
+
+module.exports = Test;
---
+++ app/index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// var routes = require('config/routes');
+var Test = require('components/Test');
+
+console.log(Test)
+ReactDOM.render(
+  <Test />,
+  document.getElementById('app')
+);
